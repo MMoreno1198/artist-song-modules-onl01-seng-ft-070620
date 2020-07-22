3f93fae121a4951a86fb5d112ca097f322fb1aa2
Marcus Moreno <legendary.1198@gmail.com>
2020-07-22 02:44:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -28,15 +28,23 @@ class Artist
   end
 
   def add_song(song)
+    
     @songs << song
     song.artist = self
+    
   end
+
 
   def add_songs(songs)
+    
     songs.each { |song| add_song(song) }
+    
   end
 
+
   def to_param
+    
     name.downcase.gsub(' ', '-')
+    
   end
 end
